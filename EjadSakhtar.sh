2e2b89111f642e009dbb4cbfd1e31f5d46bd6e0b
Razavi1999 <razavismhr@gmail.com>
2022-07-10 17:34:25 +0430

    how to create directory structure

---
+++ EjadSakhtar.sh
@@ -0,0 +1,9 @@
+mkdir quera
+mkdir -p quera/bash
+touch quera/bash/interpreter.sh quera/bash/log.txt
+
+mkdir -p quera/java
+touch quera/java/compiler.java quera/java/log.txt
+
+mkdir -p quera/python
+touch quera/python/interpreter.py quera/python/log.txt
